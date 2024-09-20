d0509a557d48e82b8d6c6d4ebc5fdc151b1444c2
Debug-001 <ombhatt1158@gmail.com>
2024-09-20 20:45:03 +0530

    async storage re-render fix

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
-import {getFirestore} from 'firebase/firestore'
-// import Constants from 'expo-constants'
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBTs8fvhAh4w0Mh-KbIrrDqNXJjhxKE3xk",
@@ -10,10 +10,14 @@ const firebaseConfig = {
   storageBucket: "valovault.appspot.com",
   messagingSenderId: "245026495894",
   appId: "1:245026495894:web:eebf0bea5d8369e835d1b7"
-  };
-  
+};
 
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
 
-initializeApp(firebaseConfig)
-export const auth = getAuth()
-export const database = getFirestore()
+// Initialize Firebase Auth with Async Storage
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
+export const database = getFirestore();
